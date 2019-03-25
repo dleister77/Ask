48047c6c6faac388f14f028887e426d92db6dac4
Doug Leister <leisterbrau@gmail.com>
2019-03-25 10:55:18 -0400

    "added review  and picture class"

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from app import db, login
+from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class State(db.Model):
+    """States used in db."""
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String(64), nullable=False, index=True)
     state_short = db.Column(db.String(24), index=True)
@@ -14,6 +16,7 @@ class State(db.Model):
 
 
 class Address(db.Model):
+    """Addresses used for users and providers."""
     id = db.Column(db.Integer, primary_key=True)
     line1 = db.Column(db.String(128))
     line2 = db.Column(db.String(128))
@@ -35,6 +38,7 @@ class User(UserMixin, db.Model):
     address_id = db.Column(db.Integer, db.ForeignKey("address.id"))
 
     address = db.relationship("Address", backref="user", lazy=True)
+    reviews = db.relationship("Review", backref="user", lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -45,24 +49,29 @@ class User(UserMixin, db.Model):
     def _repr__(self):
         return f"<User {self.username}>"
 
-    def _repr__(self):
-        return f"<Zip {self.zip}>"
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-#many to many table linking providers to multiple categories
-category_provider_table = Table('association', db.Model.metadata,
-    Column('category_id', Integer, ForeignKey('category.id')), 
-    Column('provider_id', Integer, ForeignKey('provider.id')))
+
+# many to many table linking providers to multiple categories
+category_provider_table = db.Table('association', db.Model.metadata,
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id')), 
+    db.Column('provider_id', db.Integer, db.ForeignKey('provider.id')))
+
 
 class Category(db.Model):
     """List service categories."""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    providers = db.relationship("provider", secondary=category_provider_table, 
+
+    providers = db.relationship("Provider", secondary=category_provider_table, 
                               backref="categories")
+    reviews = db.relationship("Review", backref="category", lazy=True)
+
+    def _repr__(self):
+        return f"<Category {self.category}>"
+
 
 class Provider(db.Model):
     """Create service provider record."""
@@ -74,3 +83,38 @@ class Provider(db.Model):
     address_id = db.Column(db.Integer, db.ForeignKey("address.id"))
 
     address = db.relationship("Address", backref="provider", lazy=True)
+    reviews = db.relationship("Review", backref="provider", lazy=True)
+
+    def _repr__(self):
+        return f"<Provider {self.name}>"
+
+
+# many to many table linking pictures with reviews
+review_picture = db.Table('association', db.Model.metadata,
+    db.Column('review_id', db.Integer, db.ForeignKey('review.id')), 
+    db.Column('picture_id', db.Integer, db.ForeignKey('picture.id')))
+
+
+class Review(db.Model):
+    """Class to create define review table in db."""
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    provider_id = db.Column(db.Integer, db.ForeignKey("provider.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+    rating = db.Column(db.Integer)
+    description = db.Column(db.String(120))
+    service_date = db.Column(db.Date)
+    comments = db.Column(db.Text)
+    
+    pictures = db.relationship("Picture", secondary=review_picture, 
+                               backref="review")
+
+    def _repr__(self):
+        return f"<Rating {self.id}, {self.date}>"
+
+class Picture(db.Model):
+    """Class to define picture table in db, to be linked with reviews.""""
+    id = db.Column(db.Integer, primary_key=True)
+    picture = db.Column(db.Blob)
+
