972bc5e13e42b196a606641b6830ff8ae9558fb1
dleister77 <leisterbrau@gmail.com>
2020-04-21 13:00:52 -0400

    fixed group page issues

--- app/frontend/src/pages/group/groupProfile.js
+++ app/frontend/src/pages/group/groupProfile.js
@@ -1,11 +1,21 @@
 import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import { VBTooltip } from 'bootstrap-vue';
 
+import DeviceDetectionMixin from '../../components/mixins/deviceDetection';
+import ModalFormUsermessageMixin from '../../components/modal-message-user-mixin';
+
+
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
 
 const groupProfile = new Vue({
   el: '#appContent',
   delimiters: ['[[', ']]'],
-  mounted() {
-    $('[data-toggle="tooltip"]').tooltip();
+  mixins: [DeviceDetectionMixin, ModalFormUsermessageMixin],
+  directives: {
+    'b-tooltip': VBTooltip,
   },
 });
 
